b8086a9392cdeb7faca9c937fd81658661cce2c5
Aliaksandr Dzemidovich <Aliaksandr.Dzemidovich@thomsonreuters.com>
2019-03-29 23:00:34 +0300

    Add module 3. CallNtPowerInformationWrapper

---
+++ Module3/Unmanaged.Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Unmanaged.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var t = CallNtPowerInformationWrapper.GetNtPowerInformationLastSleepTime<int>();
+        }
+    }
+}
---
+++ Module3/Unmanaged/CallNtPowerInformationWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Unmanaged.Infastructure;
+
+namespace Unmanaged
+{
+    public static class CallNtPowerInformationWrapper
+    {
+        [DllImport("powrprof.dll", SetLastError = true)]
+        private static extern uint CallNtPowerInformation(
+            int informationLevel,
+            IntPtr lpInputBuffer,
+            uint nInputBufferSize,
+            IntPtr lpOutputBuffer,
+            uint nOutputBufferSize);
+
+        public static TOutput GetNtPowerInformationLastSleepTime<TOutput>()
+        {
+            IntPtr outputBuffer = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(TOutput)));
+
+            var returnValue = CallNtPowerInformation(
+                (int)PowerInformationLevel.LastSleepTime, IntPtr.Zero, 0, 
+                outputBuffer, (uint)Marshal.SizeOf(typeof(TOutput)) * 2);
+
+            if (returnValue != (uint)NtStatus.Success)
+            {
+                throw new Win32Exception(
+                    $"{(NtStatus)Enum.Parse(typeof(NtStatus), returnValue.ToString())}");
+            }
+
+            return Marshal.PtrToStructure<TOutput>(outputBuffer);
+        }
+    }
+
+    
+}
