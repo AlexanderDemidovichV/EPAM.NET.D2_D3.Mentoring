e8a60217a58cfacd565e320fc53adfb41be8b076
Aliaksandr Dzemidovich <Aliaksandr.Dzemidovich@thomsonreuters.com>
2019-03-30 14:00:40 +0300

    Complete module 3. task 1

--- Module3/Unmanaged.Tests/Tests.cs
+++ Module3/Unmanaged.Tests/Tests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using Unmanaged.Infastructure;
 
 namespace Unmanaged.Tests
 {
@@ -12,9 +14,63 @@ namespace Unmanaged.Tests
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void GetNtPowerInformation_GetLastSleepTime_DoesNotThrow()
         {
-            var t = CallNtPowerInformationWrapper.GetNtPowerInformationLastSleepTime<int>();
+            Assert.DoesNotThrow(() => CallNtPowerInformationWrapper.
+                GetNtPowerInformationValue<long>(
+                PowerInformationLevel.LastSleepTime));
+        }
+
+        [Test]
+        public void GetNtPowerInformation_GetLastWakeTime_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => CallNtPowerInformationWrapper.
+                GetNtPowerInformationValue<long>(
+                    PowerInformationLevel.LastWakeTime));
+        }
+
+        [Test]
+        public void GetNtPowerInformation_GetSystemBatteryState_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => CallNtPowerInformationWrapper.
+                GetNtPowerInformationValue<SystemBatteryState>(
+                    PowerInformationLevel.SystemBatteryState));
+        }
+
+        [Test]
+        public void GetNtPowerInformation_GetSystemPowerInformation_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => CallNtPowerInformationWrapper.
+                GetNtPowerInformationValue<SystemPowerInformation>(
+                    PowerInformationLevel.SystemPowerInformation));
+        }
+
+        [Test]
+        public void GetNtPowerInformation_GetSystemReserveHiberFile_DoesNotThrow()
+        {
+            IntPtr outputBuffer =
+                Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(bool)));
+
+            Marshal.WriteByte(outputBuffer, 1);
+            //Trying to reserve hibernation file...
+            Assert.DoesNotThrow(() => CallNtPowerInformationWrapper.
+                GetNtPowerInformationValue<bool>(PowerInformationLevel
+                    .SystemReserveHiberFile, outputBuffer));
+
+            outputBuffer =
+                Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(bool)));
+
+            Marshal.WriteByte(outputBuffer, 0);
+            //Trying to remove hibernation file...
+            Assert.DoesNotThrow(() => CallNtPowerInformationWrapper.
+                GetNtPowerInformationValue<bool>(PowerInformationLevel
+                    .SystemReserveHiberFile, outputBuffer));
+        }
+
+        [Test]
+        public void SetSuspendState_SetHibernatesState_SystemHibernates()
+        {
+            SetSuspendStateWrapper.SetSuspendState(true, false);
         }
     }
 }
