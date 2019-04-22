29502f0ada42ca6720f747a2bb6033dfb7fbcfe1
Aliaksandr Dzemidovich <Aliaksandr.Dzemidovich@thomsonreuters.com>
2019-04-22 16:54:44 +0300

    split method for AppInsights api

--- Module4/MonitoringServer/Infastructure/HandlerHelper.cs
+++ Module4/MonitoringServer/Infastructure/HandlerHelper.cs
@@ -15,6 +15,9 @@ namespace MonitoringServer.Infastructure
         public const string SqlServerDatabaseConnectionString =
             "Server=tcp:dzemidovich-sql-server-dev.database.windows.net,1433;Initial Catalog=epam-mentoring;Persist Security Info=False;User ID=Gendalf;Password=AzureMentoringEpam2018;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
+        private const string AppInsightsAppId = "eadcb792-3725-4a03-8799-8a883abbe833";
+        private const string AppInsightsApiKey = "5f6bqdsymdwqxpiy7dib74v5z3m3dppjwdc87cga";
+
         public static IEnumerable<HandlerModel> GetAll()
         {
             using (IDbConnection db = new SqlConnection(SqlServerDatabaseConnectionString))
@@ -35,18 +38,26 @@ namespace MonitoringServer.Infastructure
 
         public static Entity GetHandlerViewModel(HandlerModel handler)
         {
-            var result = AppInsightsTelemetry.GetTelemetry
-            ("eadcb792-3725-4a03-8799-8a883abbe833", "5f6bqdsymdwqxpiy7dib74v5z3m3dppjwdc87cga",
-                $"timespan=PT1H&query=requests%7C%20where%20customDimensions.HandlerId%20%3D%3D%20%22{handler.Guid.ToString()}%22%20%7C%20summarize%20avg(duration)");
+            var duration = GetHandlerDuration(handler);
 
-            var aiResult = JsonConvert.DeserializeObject<RootObject>(result);
-
-            var duration = aiResult.Tables[0].Rows[0][0].ToString();
             return new HandlerViewModel
             {
                 Guid = handler.Guid.ToString(),
                 Duration = Convert.ToDouble(duration)
             };
+        }
+
+        private static string GetHandlerDuration(HandlerModel handler)
+        {
+            var result = AppInsightsTelemetry.GetTelemetry
+            (AppInsightsAppId, AppInsightsApiKey,
+                $"timespan=PT1H&query=requests%7C%20where%20customDimensions.HandlerId%20%3D%3D%20%22{handler.Guid.ToString()}%22%20%7C%20summarize%20avg(duration)");
+
+            var aiResult = JsonConvert.DeserializeObject<RootObject>(result);
+
+            var duration = aiResult.Tables[0].Rows[0][0].ToString();
+
+            return duration;
         }
     }
 }
