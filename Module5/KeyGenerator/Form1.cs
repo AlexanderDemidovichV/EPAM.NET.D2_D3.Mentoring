21ff0ad7b44d30f9432d06e1047395955f5cda04
Aliaksandr Dzemidovich <Aliaksandr.Dzemidovich@thomsonreuters.com>
2019-05-02 20:14:15 +0300

    Remove unnecessary code

--- Module5/KeyGenerator/Form1.cs
+++ Module5/KeyGenerator/Form1.cs
@@ -25,28 +25,11 @@ namespace KeyGenerator
 
         private int[] hello()
         {
-            var evalA = new eval_a();
             var networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
             var addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
-            evalA.a = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
-
-            var selector1 = new Func<byte, int, int>(evalA.method_a);
-            return addressBytes.Select(selector1).Select(a02 => a02 * 10).ToArray();
-        }
-
-        private sealed class eval_a
-        {
-            public byte[] a;
-
-            private int[] b;
-
-            [NonSerialized]
-            private string eval_c;
-
-            public int method_a(byte A_0, int A_1)
-            {
-                return A_0 ^ a[A_1];
-            }
+            var binaryDate = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            
+            return addressBytes.Select((b, a) => b ^ binaryDate[a]).Select(i => i * 10).ToArray();
         }
     }
 }
